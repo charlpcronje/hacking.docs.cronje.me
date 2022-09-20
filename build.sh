2fc5b3f77c5d747f53ff28795a924fca14324101
Charl Cronje <charlcp@gmail.com>
2022-09-20 18:09:16 +0200

    Update docs

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-retype build --host setup.docs.CRONje.ME
+#!/bin/bash
+if [ ! -d "./public" ] 
+then 
+    echo "Directory './public' does not exist";
+    echo "Creating Directory: './public'";
+fi
+  
+echo "Running Build";
+retype build
+cd public
+find . -name "*.html" -exec sed -i 's/m = localStorage.getItem("doc_theme")/m = "dark"/g' {} \;
+cd ..
